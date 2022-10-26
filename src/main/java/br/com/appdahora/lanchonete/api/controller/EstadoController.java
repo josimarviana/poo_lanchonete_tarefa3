e76c40e69541d466991ee89ca54f3d897c3a8e39
Josimar Viana Silva <josimarviana@gmail.com>
2022-10-26 01:00:49 -0300

    atualizando parcial estado

--- src/main/java/br/com/appdahora/lanchonete/api/controller/EstadoController.java
+++ src/main/java/br/com/appdahora/lanchonete/api/controller/EstadoController.java
@@ -5,13 +5,17 @@ import br.com.appdahora.lanchonete.domain.exception.EntidadeNaoEncontradaExcepti
 import br.com.appdahora.lanchonete.domain.model.Estado;
 import br.com.appdahora.lanchonete.domain.repository.EstadoRepository;
 import br.com.appdahora.lanchonete.domain.service.CadastroEstadoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -59,6 +63,36 @@ public class EstadoController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+    @PatchMapping("/{estadoId}")
+    public ResponseEntity<?> atualizarParcial(@PathVariable Long estadoId, @RequestBody Map<String, Object> campos){
+        //Map para maior controle de chave e valor, onde valor é um objeto
+        Optional<Estado> estadoAtual =  estadoRepository.findById(estadoId); //recuperando do banco
+
+        if (!estadoAtual.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+
+        merge(campos, estadoAtual.get());
+
+        return atualizar(estadoId, estadoAtual.get());
+
+    }
+
+    private static void merge(Map<String, Object> dadosOrigem, Estado estadoDestino) {
+        ObjectMapper objectMapper = new ObjectMapper(); // converte json em objetos
+        Estado estadoOrigem = objectMapper.convertValue(dadosOrigem, Estado.class); // cria um objeto a partir do json
+
+        dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+            //valorPropriedade não é devidamente convertido para os tipos corretos, dá erro ao atribuir um bigint pq no json lê int
+            Field field = ReflectionUtils.findField(Estado.class, nomePropriedade); //recupera a instância dos atributos
+            field.setAccessible(true); //permite acesso aos membros da classe, mesmo sendo privados
+
+            Object novoValor = ReflectionUtils.getField(field, estadoOrigem); //recupera o valos dos atributos
+
+            ReflectionUtils.setField(field, estadoDestino, novoValor); //atribui os valores
+
+        });
     }
 
     @DeleteMapping("/{estadoId}")
